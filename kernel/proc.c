ef878e5895ec313ee43ffd192e2e8b20665e8ae9
oasg <gouruibo98765@outlook.com>
2024-01-07 13:21:13 +0800

    finish pgtbl -Speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -157,9 +157,12 @@ freeproc(struct proc *p)
 {
   if(p->trapframe)
     kfree((void*)p->trapframe);
+  if(p->pgtacc)
+    kfree((void*)p->pgtacc);
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
+
   p->pagetable = 0;
   p->sz = 0;
   p->pid = 0;
@@ -201,6 +204,22 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
+  //user syscall page
+  //read only
+   // Allocate a trapframe page.
+  if ((p->pgtacc = (struct usyscall*)kalloc()) == 0)
+  {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  if(mappages(pagetable,USYSCALL,PGSIZE,(uint64)(p->pgtacc),PTE_R|PTE_W|PTE_U)<0) {
+    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+  p->pgtacc->pid= p->pid;
+  
 
   return pagetable;
 }
@@ -212,6 +231,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable,USYSCALL,1,0);
   uvmfree(pagetable, sz);
 }
 
